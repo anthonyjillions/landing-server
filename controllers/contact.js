8743eec1d9c5dcecf3f1de9db80785a16a8d2451
anthonyjillions <anthonyjillions@gmail.com>
2016-05-03 15:25:02 -0400

    init

--- controllers/contact.js
+++ controllers/contact.js
@@ -1,57 +1,35 @@
-var nodemailer = require("nodemailer");
-var transporter = nodemailer.createTransport({
-  service: 'SendGrid',
+const nodemailer = require('nodemailer');
+const transporter = nodemailer.createTransport({
+  service: 'MailGun',
   auth: {
-    user: process.env.SENDGRID_USER,
-    pass: process.env.SENDGRID_PASSWORD
-  }
+    user: process.env.MAILGUN_USER,
+    pass: process.env.MAILGUN_PASSWORD,
+  },
 });
 
-/**
- * GET /contact
- * Contact form page.
- */
-exports.getContact = function(req, res) {
-  res.render('contact', {
-    title: 'Contact'
-  });
-};
+exports.postContact = (req, res) => {
+  const from = req.body.email;
+  const name = req.body.name;
+  const body = req.body.message;
+  const to = 'anthonyjillions@gmail.com';
+  const subject = name;
 
-/**
- * POST /contact
- * Send a contact form via Nodemailer.
- */
-exports.postContact = function(req, res) {
-  req.assert('name', 'Name cannot be blank').notEmpty();
-  req.assert('email', 'Email is not valid').isEmail();
-  req.assert('message', 'Message cannot be blank').notEmpty();
-
-  var errors = req.validationErrors();
-
-  if (errors) {
-    req.flash('errors', errors);
-    return res.redirect('/contact');
-  }
-
-  var from = req.body.email;
-  var name = req.body.name;
-  var body = req.body.message;
-  var to = 'your@email.com';
-  var subject = 'Contact Form | Hackathon Starter';
-
-  var mailOptions = {
-    to: to,
-    from: from,
-    subject: subject,
-    text: body
+  const mailOptions = {
+    to,
+    from,
+    subject,
+    text: body,
   };
 
-  transporter.sendMail(mailOptions, function(err) {
+  transporter.sendMail(mailOptions, (err) => {
     if (err) {
-      req.flash('errors', { msg: err.message });
-      return res.redirect('/contact');
+      res.send({
+        message: 'Uh oh. Something went wrong please try again',
+      });
     }
-    req.flash('success', { msg: 'Email has been sent successfully!' });
-    res.redirect('/contact');
+
+    res.send({
+      message: 'Hey, thanks for sending a message. I\'ll get back to you as soon as possible.',
+    });
   });
 };

1489ed492578c1aece344d6737587b570ddb38b0
anthonyjillions <anthonyjillions@gmail.com>
2016-05-04 23:29:15 -0400

    set origin

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Origin", "https://www.anthonyjillions.com");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
